60a467dec27af70761e140785a762cf5856c905a
Five CHEN <fivechen@FivedeMacBook-Pro.local>
2018-11-16 22:01:36 +0800

    0.0.2

---
+++ products/EbluAdvertisementToolkit.framework/Headers/EbluAdvertisementConstant.h
@@ -0,0 +1,13 @@
+//
+//  EbluAdvertisementConstant.h
+//  EbluAdvertisementToolkit
+//
+//  Created by Five CHEN on 2018/11/15.
+//  Copyright © 2018年 eblu. All rights reserved.
+//
+
+#ifndef EbluAdvertisementConstant_h
+#define EbluAdvertisementConstant_h
+
+#define IntersectAdReceivedNotification @"IntersectAdReceivedNotification"
+#endif /* EbluAdvertisementConstant_h */
